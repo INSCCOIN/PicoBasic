812f7b5344443b55c3c2e29dbadadc0be447c0c6
Ian Simpson <53959046+INSCCOIN@users.noreply.github.com>
2025-07-10 15:22:26 -0400

    Update converter.bat

--- Applications/PicoPub/converter.bat
+++ Applications/PicoPub/converter.bat
@@ -1,6 +1,6 @@
 @echo off
 REM INSCCOIN 2025
-
+REM Usage: converter.bat input.txt output.txt
 
 if "%~2"=="" (
     echo Usage: %0 input.txt output.txt
@@ -14,18 +14,28 @@ if exist "%outfile%" del "%outfile%"
 
 for /f "usebackq delims=" %%L in ("%infile%") do call :wrapline "%%L"
 
-REM Add signature line at the end
->>"%outfile%" echo INSCCOIN Converter 1.0
+REM github signature at eof
+>>"%outfile%" echo INSCCOIN Converter 2.0
 
 goto :eof
 
 :wrapline
 set "line=%~1"
 :wraploop
+if "!line!"=="" exit /b
 set "chunk=!line:~0,36!"
->>"%outfile%" echo(!chunk!
-set "line=!line:~36!"
-if not "!line!"=="" goto wraploop
-exit /b
-
-echo Done. Output: %outfile%
+REM break at 36 or space before 
+for /l %%i in (36,-1,1) do (
+    if not defined breakpos if "!chunk:~%%i,1!"==" " set "breakpos=%%i"
+)
+if defined breakpos (
+    set "out=!chunk:~0,%breakpos%!"
+    set "line=!line:~%breakpos%!"
+    set "line=!line:~1!"  REM Remove leading space
+    set "breakpos="
+) else (
+    set "out=!chunk!"
+    set "line=!line:~36!"
+)
+>>"%outfile%" echo(!out!
+goto wraploop
